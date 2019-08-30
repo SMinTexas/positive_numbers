4a9371cf5d58b25f3939bc1edd2b2746811c33b1
Steven Murray <tstevenm@gmail.com>
2019-08-30 14:30:18 -0500

    Initial Commit

---
+++ positives.py
@@ -0,0 +1,7 @@
+# Given a list of numbers, print each number in the list that is great than zero.
+
+numbers = [-2, 0, -66, 100, 1, -1, -67, -79, 0, 90, 85]
+
+for number in numbers:
+    if number > 0:
+        print(number)
